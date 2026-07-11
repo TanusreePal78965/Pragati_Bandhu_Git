54894acd49f658b2ee095170b833528d65d01929
shubhadeep29 <shhubhadeep.3g@gmail.com>
2026-07-11 12:35:35 +0530

    Fix vite base path for new repo

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Pragati_Bandhu/',
+  base: '/Pragati_Bandhu_Git/',
   plugins: [react()],
 })
